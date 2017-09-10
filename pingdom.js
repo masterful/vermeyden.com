9cf0bc57c71c390d47046630c262dac8f909c66e
Pim Vermeyden <pvermeyden@gmail.com>
2017-09-10 12:19:24 -0400

    copy over from current site

---
+++ pingdom.js
@@ -0,0 +1,9 @@
+var _prum = [['id', '550ad029abe53d581ac6c9d2'],
+  ['mark', 'firstbyte', (new Date()).getTime()]];
+(function() {
+  var s = document.getElementsByTagName('script')[0]
+    , p = document.createElement('script');
+  p.async = 'async';
+  p.src = '//rum-static.pingdom.net/prum.min.js';
+  s.parentNode.insertBefore(p, s);
+})();
